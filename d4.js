d1f199af9b902fcf8f6c4f13ab9b6a51f5031205
hamzaabrar <80463417+hamzaabrar@users.noreply.github.com>
2021-03-11 14:33:25 +0000

    Update d4.js

--- d4.js
+++ d4.js
@@ -9,7 +9,13 @@ ASSIGNMENT RULES
 /* EXERCISE 1
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
+const area = function(l1,l2) {
+    let length = l1 * l2
 
+    return length
+}
+
+// console.log(area(5,5))
 /* EXERCISE 2
 Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
